26d1baf33acb77d7d4c17a8a656863805924bdb7
Narayan Sharma <developer@edynamics.co.in>
2020-07-23 17:02:08 +0530

    Modify data for latest version

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 const { Router } = express;
 const router = Router();
 router.get('/', (req, res) => {
-    res.send('this is index page')
+    res.send('this is updated index page')
 })
 router.get('/users', (req, res) => {
-    res.send([{ 'name': 'Narayan Sharma' }])
+    res.send([{ 'name': 'Narayan Sharma' }, { 'name': 'Siva Sharma' }, { 'name': 'Santosh Sharma' }{ 'name': 'Prakash Sharma' }])
 })
 app.use('/api', router);
 app.listen(80);
